eda432f2c6fcb460678908973f39a867ada60ef0
malgeum <kwonsunny@naver.com>
2025-10-03 15:20:50 +0900

    크롤링 뉴스기사

--- 뉴스데이터수집및엑셀(한겨레).py
+++ 뉴스데이터수집및엑셀(한겨레).py
@@ -16,7 +16,7 @@ temp_profile_dir = tempfile.mkdtemp() # <- solution
 
 opt = webdriver.ChromeOptions()
 opt.add_argument(f"--user-data-dir={temp_profile_dir}")
-#opt.add_argument('--headless')
+opt.add_argument('--headless')
 opt.add_experimental_option("prefs", {
     "profile.managed_default_content_settings.images": 2,
     "profile.managed_default_content_settings.plugins": 2
@@ -39,73 +39,136 @@ wb = Workbook()
 ws = wb.active
 excelnum = 1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/politics?page={num}')
+    if num >= 335 :
+        break
+    try:
+
+        browser.get(f'https://www.hani.co.kr/arti/politics?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
+
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
+   # href_list2 = [el.get_attribute('href') for el in results2]
+    for a in href_list:
+        try:
+            browser.get(a)
+            time.sleep(5)
+            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+        
+            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+
+            for i in splitresult:
+                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
+                    ws[f"A{excelnum+1}"] = i 
+                    excelnum+=1
+
+        except Exception as e:
+            print(f"Error at {e}")
+            continue
+    if num >= 334 :
+        break
+    num+=1
+
+
+num=1
+while True :
+    if num >= 335 :
+        break
+    try:
+
+        browser.get(f'https://www.hani.co.kr/arti/society?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
+
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
+   # href_list2 = [el.get_attribute('href') for el in results2]
+    for a in href_list:
+        try:
+            browser.get(a)
+            time.sleep(5)
+            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+        
+            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+
+            for i in splitresult:
+                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
+                    ws[f"A{excelnum+1}"] = i 
+                    excelnum+=1
+
+        except Exception as e:
+            print(f"Error at {e}")
+            continue
+    if num >= 334 :
+        break
+    num+=1
+
+num=1
+while True :
+    if num >= 335 :
+        break
+    try:
+
+        browser.get(f'https://www.hani.co.kr/arti/area?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
+
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
+   # href_list2 = [el.get_attribute('href') for el in results2]
+    for a in href_list:
+        try:
+            browser.get(a)
+            time.sleep(5)
+            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+        
+            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+
+            for i in splitresult:
+                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
+                    ws[f"A{excelnum+1}"] = i 
+                    excelnum+=1
+
+        except Exception as e:
+            print(f"Error at {e}")
+            continue
+    if num >= 334 :
+        break
+    num+=1
     
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
-   # href_list2 = [el.get_attribute('href') for el in results2]
-    for a in href_list:
-        try:
-            browser.get(a)
-            time.sleep(5)
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
-            for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
-        except Exception as e:
-            continue
-    if num == 334 :
-        break
-    num+=1
-
-
 num=1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/society?page={num}')
- 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
-   # href_list2 = [el.get_attribute('href') for el in results2]
-    for a in href_list:
-        try:
-            browser.get(a)
-            time.sleep(5)
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
-            for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
-        except Exception as e:
-            continue
-    if num == 334 :
+    if num >= 335 :
         break
-    num+=1
+    try:
 
+        browser.get(f'https://www.hani.co.kr/arti/economy?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
 
-num=1
-while True :
-    browser.get(f'https://www.hani.co.kr/arti/area?page={num}')
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
    # href_list2 = [el.get_attribute('href') for el in results2]
     for a in href_list:
         try:
@@ -121,51 +184,29 @@ while True :
                     excelnum+=1
 
         except Exception as e:
+            print(f"Error at {e}")
             continue
-    if num == 334 :
+    if num >= 334 :
         break
     num+=1
 
 num=1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/economy?page={num}')
-
-
-    #results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA >a' )
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
-   # href_list2 = [el.get_attribute('href') for el in results2]
-    for a in href_list:
-        try:
-            browser.get(a)
-            time.sleep(5)
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
-            for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
-        except Exception as e:
-            continue
-    if num == 334 :
+    if num >= 335 :
         break
-    num+=1
+    try:
 
+        browser.get(f'https://www.hani.co.kr/arti/international?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
 
-num=1
-while True :
-    browser.get(f'https://www.hani.co.kr/arti/international?page={num}')
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
    # href_list2 = [el.get_attribute('href') for el in results2]
     for a in href_list:
         try:
@@ -181,19 +222,29 @@ while True :
                     excelnum+=1
 
         except Exception as e:
+            print(f"Error at {e}")
             continue
-    if num == 334 :
+    if num >= 334 :
         break
     num+=1
 
 num=1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/culture?page={num}')
- 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCardVertical_card__LLa8l >a')))
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+    if num >= 335 :
+        break
+    try:
 
-    href_list = [el.get_attribute('href') for el in results]
+        browser.get(f'https://www.hani.co.kr/arti/culture?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCardVertical_card__LLa8l >a')))
+
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
    # href_list2 = [el.get_attribute('href') for el in results2]
     for a in href_list:
         try:
@@ -209,19 +260,67 @@ while True :
                     excelnum+=1
 
         except Exception as e:
+            print(f"Error at {e}")
             continue
-    if num == 334 :
+    if num >= 334 :
+        break
+    num+=1
+num=1
+
+while True :
+    if num >= 335 :
+        break
+    try:
+
+        browser.get(f'https://www.hani.co.kr/arti/science?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
+
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
+   # href_list2 = [el.get_attribute('href') for el in results2]
+    for a in href_list:
+        try:
+            browser.get(a)
+            time.sleep(5)
+            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+        
+            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+
+            for i in splitresult:
+                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
+                    ws[f"A{excelnum+1}"] = i 
+                    excelnum+=1
+
+        except Exception as e:
+            print(f"Error at {e}")
+            continue
+    if num >= 334 :
         break
     num+=1
 
 num=1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/science?page={num}')
+    if num >= 335 :
+        break
+    try:
 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+        browser.get(f'https://www.hani.co.kr/arti/animalpeople?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.ArticleListTitle_link__U_6Bo')))
 
-    href_list = [el.get_attribute('href') for el in results]
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
    # href_list2 = [el.get_attribute('href') for el in results2]
     for a in href_list:
         try:
@@ -237,19 +336,29 @@ while True :
                     excelnum+=1
 
         except Exception as e:
+            print(f"Error at {e}")
             continue
-    if num == 334 :
+    if num >= 334 :
         break
     num+=1
 
 num=1
 while True :
-    browser.get(f'https://www.hani.co.kr/arti/animalpeople?page={num}')
+    if num >= 335 :
+        break
+    try:
 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.ArticleListTitle_link__U_6Bo')))
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+        browser.get(f'https://www.hani.co.kr/arti/opinion?page={num}')
+        
+        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
 
-    href_list = [el.get_attribute('href') for el in results]
+        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+
+        href_list = [el.get_attribute('href') for el in results]
+    except Exception as e :
+        num+=1
+        print('에러' ,e)
+        continue
    # href_list2 = [el.get_attribute('href') for el in results2]
     for a in href_list:
         try:
@@ -265,39 +374,12 @@ while True :
                     excelnum+=1
 
         except Exception as e:
+            print(f"Error at {e}")
             continue
-    if num == 334 :
+    if num >= 334 :
         break
     num+=1
 
-num=1
-while True :
-    browser.get(f'https://www.hani.co.kr/arti/opinion?page={num}')
-
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-    #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
-    href_list = [el.get_attribute('href') for el in results]
-   # href_list2 = [el.get_attribute('href') for el in results2]
-    for a in href_list:
-        try:
-            browser.get(a)
-            time.sleep(5)
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
-            for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
-        except Exception as e:
-            continue
-    if num == 334 :
-        break
-    num+=1
-
-wb.save(f'뉴스기사수집{datetime.now().date()}.xlsx')
+wb.save(f'뉴스기사수집(한겨레){datetime.now().date()}.xlsx')
 
 
