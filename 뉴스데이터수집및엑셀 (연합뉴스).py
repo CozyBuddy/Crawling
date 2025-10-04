465471d9f8f24a3dcc61577ce924a460c69074c4
malgeum <kwonsunny@naver.com>
2025-10-04 13:41:17 +0900

    크롤링수정 연합뉴스

--- 뉴스데이터수집및엑셀 (연합뉴스).py
+++ 뉴스데이터수집및엑셀 (연합뉴스).py
@@ -12,45 +12,61 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import tempfile
-opt = webdriver.ChromeOptions()
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
 temp_profile_dir = tempfile.mkdtemp() # <- solution
-opt.add_argument(f"--user-data-dir={temp_profile_dir}")
-opt.add_argument('--headless')
+
+opt = webdriver.ChromeOptions()
+opt.set_capability("pageLoadStrategy", "eager")
+# opt.add_argument(f"--user-data-dir={temp_profile_dir}")
+#opt.add_argument('--headless')
+opt.add_argument("--disable-extensions")
+opt.add_argument("--disable-application-cache")
 opt.add_experimental_option("prefs", {
     "profile.managed_default_content_settings.images": 2,
-    "profile.managed_default_content_settings.plugins": 2
+    "profile.managed_default_content_settings.plugins": 2,
+    "profile.default_content_setting_values.notifications": 2 
 })
+
+opt.add_argument("--disable-popup-blocking")  # 팝업 차단
+opt.add_argument("--disable-notifications")  # 알림 완전 차단
 
 # opt.add_argument(f"--user-data-dir=/tmp/chrome_data_{debug_port}") 
 # opt.add_argument(f"--remote-debugging-port={debug_port}")
 
-
 opt.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                     "AppleWebKit/537.36 (KHTML, like Gecko) "
-                     "Chrome/140.0.7339.208 Safari/537.36")
+                 "AppleWebKit/537.36 (KHTML, like Gecko) "
+                 "Chrome/119.0.6045.200 Safari/537.36")
+
 opt.add_argument("--disable-blink-features=AutomationControlled"); 
-browser = webdriver.Chrome(options=opt)
-browser.set_page_load_timeout(300)
+
+browser = webdriver.Chrome(options=opt )
+browser.set_script_timeout(3)
+browser.set_page_load_timeout(5)
 browser.implicitly_wait(10)
+wait = WebDriverWait(browser, 10)
 
 
 ## 연합뉴스
-wb2 = Workbook()
-ws2 = wb2.active
-excelnum2 = 1
 
 
-browser.get(f'https://www.yna.co.kr/sitemap/')
+num =1 
+browser.get(f'https://www.yna.co.kr/sitemap/index')
 
 
 results = browser.find_elements(By.CSS_SELECTOR , 'div.link-field01 ul.link-zone01 a' )
 #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+time.sleep(3)
 href_list = [el.get_attribute('href') for el in results]
 # href_list2 = [el.get_attribute('href') for el in results2]
 for a in href_list:
-    
+    wb2 = Workbook()
+    ws2 = wb2.active
+    excelnum2 = 1
     browser.get(a)
+    time.sleep(1.5)
     result3 = browser.find_elements( By.CSS_SELECTOR  , 'div.link-zone02 a')
     #print(result3.text)
 
@@ -58,19 +74,26 @@ for a in href_list:
     for i in href_list:
 
         browser.get(i)
+        time.sleep(1.5)
         result3 = browser.find_elements(By.CSS_SELECTOR , 'div.link-zone03 a')
 
         href_list2 = [el.get_attribute('href') for el in result3]
         for i in href_list2:
-            browser.get(i)
-            time.sleep(5)
-            result3 = browser.find_element(By.CSS_SELECTOR , 'div.story-news.article')
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+            try:
+                browser.get(i)
+                time.sleep(1.5)
+                result3 = browser.find_element(By.CSS_SELECTOR , 'div.story-news.article')
+                splitresult = [s.strip()for s in result3.text.split('.') if s.strip() ]
 
-            for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws2[f"A{excelnum2+1}"] = i 
-                    excelnum2+=1
+                for i in splitresult:
+                    if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
+                        ws2[f"A{excelnum2+1}"] = i 
+                        excelnum2+=1
+            except Exception as e: 
+                continue
 
-wb2.save(f'뉴스기사수집(연합뉴스){datetime.now().date()}.xlsx')     
+    wb2.save(f'뉴스기사수집(연합뉴스){num}{datetime.now().date()}.xlsx')
+    num+=1
+
+   
 
--- 뉴스데이터수집및엑셀(한겨레).py
+++ 뉴스데이터수집및엑셀(한겨레).py
@@ -12,28 +12,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import tempfile
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
 temp_profile_dir = tempfile.mkdtemp() # <- solution
 
 opt = webdriver.ChromeOptions()
-opt.add_argument(f"--user-data-dir={temp_profile_dir}")
+opt.set_capability("pageLoadStrategy", "eager")
+# opt.add_argument(f"--user-data-dir={temp_profile_dir}")
 #opt.add_argument('--headless')
+opt.add_argument("--disable-extensions")
+opt.add_argument("--disable-application-cache")
 opt.add_experimental_option("prefs", {
     "profile.managed_default_content_settings.images": 2,
-    "profile.managed_default_content_settings.plugins": 2
+    "profile.managed_default_content_settings.plugins": 2,
+    "profile.default_content_setting_values.notifications": 2 
 })
+
+opt.add_argument("--disable-popup-blocking")  # 팝업 차단
+opt.add_argument("--disable-notifications")  # 알림 완전 차단
+
 # opt.add_argument(f"--user-data-dir=/tmp/chrome_data_{debug_port}") 
 # opt.add_argument(f"--remote-debugging-port={debug_port}")
 
 opt.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                     "AppleWebKit/537.36 (KHTML, like Gecko) "
-                     "Chrome/140.0.7339.208 Safari/537.36")
+                 "AppleWebKit/537.36 (KHTML, like Gecko) "
+                 "Chrome/119.0.6045.200 Safari/537.36")
 
 opt.add_argument("--disable-blink-features=AutomationControlled"); 
-browser = webdriver.Chrome(options=opt)
-browser.set_page_load_timeout(300)
+
+browser = webdriver.Chrome(options=opt )
+browser.set_script_timeout(3)
+browser.set_page_load_timeout(5)
 browser.implicitly_wait(10)
 wait = WebDriverWait(browser, 10)
-
 num = 1
 wb = Workbook()
 ws = wb.active
@@ -42,12 +55,11 @@ while True :
     if num >= 335 :
         break
     try:
-
         browser.get(f'https://www.hani.co.kr/arti/politics?page={num}')
-        
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
 
-        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+        #results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.BaseArticleCard_content__tYkEA > a[href]")))
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
 
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
@@ -59,35 +71,45 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            #result3 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+            
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
     if num >= 334 :
         break
+
     num+=1
 
-
+wb.save(f'뉴스기사수집(한겨레){datetime.now().date()}.xlsx')
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+wb2 = Workbook()
+ws2 = wb2.active
+excelnum = 1
 num=1
 while True :
-    if num >= 335 :
+    if num >= 334 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/society?page={num}')
         
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
+        # results = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.BaseArticleCard_content__tYkEA > a[href]")))
+        # #time.sleep(3)
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
         #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        #results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -98,15 +120,17 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
+            #time.sleep(3)
 
+            #result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -114,17 +138,22 @@ while True :
         break
     num+=1
 
+wb2.save(f'뉴스기사수집(한겨레)2{datetime.now().date()}.xlsx')
+
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+wb3 = Workbook()
+ws3 = wb3.active
 num=1
+excelnum = 1
 while True :
-    if num >= 335 :
+    if num >= 334 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/area?page={num}')
-        
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
 
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
@@ -136,34 +165,37 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
     if num >= 334 :
         break
     num+=1
-    
+
+wb3.save(f'뉴스기사수집(한겨레)3{datetime.now().date()}.xlsx')
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+excelnum = 1
+wb4 = Workbook()
+ws4 = wb4.active
 num=1
 while True :
-    if num >= 335 :
+    if num >= 334 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/economy?page={num}')
-        
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -174,15 +206,15 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -190,18 +222,21 @@ while True :
         break
     num+=1
 
+wb4.save(f'뉴스기사수집(한겨레)4{datetime.now().date()}.xlsx')
+excelnum = 1
+wb5 = Workbook()
+ws5 = wb5.active
 num=1
 while True :
-    if num >= 335 :
+    if num >= 334 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/international?page={num}')
         
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
-        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
+        
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -212,15 +247,15 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -228,18 +263,22 @@ while True :
         break
     num+=1
 
+wb5.save(f'뉴스기사수집(한겨레)5{datetime.now().date()}.xlsx')
+
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+excelnum = 1
 num=1
 while True :
-    if num >= 335 :
+    if num >= 334 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/culture?page={num}')
         
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCardVertical_card__LLa8l >a')))
-
-        #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        #results = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.BaseArticleCardVertical_card__LLa8l > a[href]")))
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCardVertical_card__LLa8l > a' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -250,15 +289,15 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+       
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -267,17 +306,23 @@ while True :
     num+=1
 num=1
 
+excelnum = 1
+
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+wb6 = Workbook()
+ws6 = wb6.active
 while True :
     if num >= 335 :
         break
     try:
 
         browser.get(f'https://www.hani.co.kr/arti/science?page={num}')
-        
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
 
+        #results = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.BaseArticleCard_content__tYkEA > a[href]")))
         #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -288,15 +333,15 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -304,6 +349,14 @@ while True :
         break
     num+=1
 
+wb6.save(f'뉴스기사수집(한겨레)6{datetime.now().date()}.xlsx')
+
+excelnum = 1
+
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+wb7 = Workbook()
+ws7 = wb7.active
 num=1
 while True :
     if num >= 335 :
@@ -312,10 +365,11 @@ while True :
 
         browser.get(f'https://www.hani.co.kr/arti/animalpeople?page={num}')
         
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a.ArticleListTitle_link__U_6Bo')))
-
+        
+        # results = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "a.ArticleListTitle_link__U_6Bo[href]")))
         #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'a.ArticleListTitle_link__U_6Bo' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -326,15 +380,16 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+         
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -342,6 +397,13 @@ while True :
         break
     num+=1
 
+wb7.save(f'뉴스기사수집(한겨레)7{datetime.now().date()}.xlsx')
+excelnum = 1
+
+browser.quit()
+browser = webdriver.Chrome(options=opt)
+wb8 = Workbook()
+ws8 = wb8.active
 num=1
 while True :
     if num >= 335 :
@@ -350,10 +412,10 @@ while True :
 
         browser.get(f'https://www.hani.co.kr/arti/opinion?page={num}')
         
-        results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.BaseArticleCard_content__tYkEA >a')))
-
+        #results = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "div.BaseArticleCard_content__tYkEA > a[href]")))
         #results2 = browser.find_elements(By.CSS_SELECTOR , 'div.section_article._TEMPLATE div.sa_text > a' )
-
+        time.sleep(3)
+        results = browser.find_elements(By.CSS_SELECTOR , 'div.BaseArticleCard_content__tYkEA > a' )
         href_list = [el.get_attribute('href') for el in results]
     except Exception as e :
         num+=1
@@ -364,15 +426,15 @@ while True :
         try:
             browser.get(a)
             
-            result3 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.article-text')))
-        
-            splitresult = [s.strip().replace('=','').replace('@','') for s in result3.text.split('\n') if s.strip() ]
-
+            time.sleep(1.5)
+            result3 = browser.find_element(By.CSS_SELECTOR , 'div.article-text' )
+            splitresult = [s.strip() for s in result3.text.split('.') if s.strip() ]
+            #.replace('=','').replace('@','')
             for i in splitresult:
-                if re.fullmatch(r"[가-힣.,!=?\"'()\ ]+", i) :
-                    ws[f"A{excelnum+1}"] = i 
-                    excelnum+=1
-
+               # if re.fullmatch(r"[가-힣0-9.·,!=?\"'()\-\ ]+", i) :
+                ws[f"A{excelnum+1}"] = i 
+                excelnum+=1
+        
         except Exception as e:
             print(f"Error at {e}")
             continue
@@ -380,6 +442,6 @@ while True :
         break
     num+=1
 
-wb.save(f'뉴스기사수집(한겨레){datetime.now().date()}.xlsx')
+wb8.save(f'뉴스기사수집(한겨레)8{datetime.now().date()}.xlsx')
 
 
